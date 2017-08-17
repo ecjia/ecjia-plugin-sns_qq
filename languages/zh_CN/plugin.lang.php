d790ae9974dcc97695e08d0657267ab36dbc3a5a
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-08-17 14:33:55 +0800

    更新新的登录插件语言包

--- languages/zh_CN/plugin.lang.php
+++ languages/zh_CN/plugin.lang.php
@@ -49,11 +49,12 @@
  */
 defined('IN_ECJIA') or exit('No permission resources.');
 
-$LANG['sns_qq'] 				= 'QQ帐号登录';
-$LANG['sns_qq_desc'] 			= '腾讯QQ（QQ）第三方帐号登录。<br/>登录接口：在线即可开通，<font color="red"><b>免费。</b></font><br/><a href="http://connect.qq.com/intro/login/" target="_blank"><font color="red">立即在线申请</font></a>';
-$LANG['sns_qq_appid'] 			= 'AppID';
-$LANG['sns_qq_appkey'] 		    = 'AppKey';
-$LANG['sns_qq_callback'] 		= '回调地址';
-//scope 授权列表
+return array(
+    'sns_qq' 				=> 'QQ帐号登录',
+    'sns_qq_desc' 			=> '腾讯QQ（QQ）第三方帐号登录。<br/>登录接口：在线即可开通，<font color="red"><b>免费。</b></font><br/><a href="http://connect.qq.com/intro/login/" target="_blank"><font color="red">立即在线申请</font></a>',
+    'sns_qq_appid' 			=> 'AppID',
+    'sns_qq_appkey' 		=> 'AppKey',
+    'sns_qq_callback' 		=> '回调地址',
+);
 
 // end
